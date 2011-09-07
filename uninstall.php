b71a3b7e7733d547caf8d99535b369bdaf44cafb
Ste_95 <Ste_95@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-07 18:28:02 +0000

    Stats are not generated during installation anymore. This is to prevent the plugin hanging on activation due to large databases.
    git-svn-id: https://plugins.svn.wordpress.org/post-pay-counter/tags/1.1.2@434822 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,40 @@
+<?php
+
+//Uninstall must have been triggered by wordpress, otherwise exit
+if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
+    exit;
+
+global $wpdb;
+
+//Delete plugin's columns in wp_posts and the plugin's custom table
+//NOTE THAT UNISTALLING THE PLUGIN DELETES ALL THE SAVED SETTINGS
+function post_pay_counter_uninstall_procedure() {
+    global $wpdb;
+    
+    $wpdb->query( 'ALTER TABLE '.$wpdb->posts.' DROP post_pay_counter' );
+    $wpdb->query( 'ALTER TABLE '.$wpdb->posts.' DROP post_pay_counter_count' );
+    $wpdb->query( 'DROP TABLE '.$wpdb->prefix.'post_pay_counter' );
+}
+
+//If working on a multisite blog
+if ( function_exists( 'is_multisite' ) AND is_multisite() ) {
+    
+	//Get all blog ids; foreach them and call the uninstall procedure on each of them
+	$blog_ids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM ".$wpdb->blogs));
+	
+    //Get all blog ids; foreach them and call the install procedure on each of them if the plugin table is found
+    foreach ( $blog_ids as $blog_id ) {
+		switch_to_blog( $blog_id );
+        
+        if( $wpdb->query( 'SHOW TABLES FROM '.$wpdb->dbname.' LIKE "'.$wpdb->prefix.'post_pay_counter"' ) )
+            post_pay_counter_uninstall_procedure();
+            
+	}
+    
+    //Go back to the main blog and return - so that if not multisite or not network activation, run the procedure once
+	restore_current_blog();
+	return;
+}
+post_pay_counter_uninstall_procedure();	
+
+?>
